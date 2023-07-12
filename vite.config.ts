4eb70310515fa8562fd774479a814f979f85a70f
Mark Manley <mark.manley3@icloud.com>
2023-07-12 01:09:28 -0400

    make static site

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@
 /// <reference types="vite/client" />
 
 import react from '@vitejs/plugin-react';
+import million from 'million/compiler';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [million.vite(), react()],
   test: {
     globals: true,
     environment: 'jsdom',
